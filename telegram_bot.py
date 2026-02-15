d6459fc80305a7100db52ed0fbd53805cb081933
Kirill Soloshenko <k.soloshenko1@mts.ai>
2026-02-15 21:43:56 +0300

    update chatbot logic

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,19 +1,29 @@
 import telebot
 from src import ChatBot
+import pathlib
 
-TOKEN = 'TOKEN'
+CURDIR = pathlib.Path(__file__).parent.resolve()
+TOKENPATH = CURDIR / ".token"
+assert TOKENPATH.exists()
+with open(TOKENPATH, "r") as file:
+    TOKEN = file.read().strip()
+assert TOKEN
+
 bot = telebot.TeleBot(TOKEN)
-chatbot = ChatBot()
+chatbot = ChatBot(use_vllm=True)
 
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
-	bot.reply_to(message, 'Hello, how are you doing?')
-	
-    
-@bot.message_handler(commands=['help'])
+    bot.reply_to(message, "Hello, how are you doing?")
+
+
+@bot.message_handler(commands=["help"])
 def help(message):
-	bot.reply_to(message, 'I am a multiagent personalized chatbot. Just send me a message!')
-	
+    bot.reply_to(
+        message, "I am a multiagent personalized chatbot. Just send me a message!"
+    )
+
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
@@ -22,10 +32,9 @@ def handle_message(message):
         reply = chatbot.response(user_query)
         bot.reply_to(message, reply)
     except Exception as e:
-        bot.reply_to(message, 'error')
-        print(f'[ERROR] {e}')
+        bot.reply_to(message, "error")
+        print(f"[ERROR] {e}")
 
 
-if __name__ == '__main__':
-    print('!!!')
+if __name__ == "__main__":
     bot.infinity_polling()
